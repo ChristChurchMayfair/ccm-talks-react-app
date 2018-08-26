aba750d38dd46595c586188d240fcacab8ee3574
Ben Robinson <ben@joinfoodchain.com>
2018-08-26 10:20:29 +0100

    Fix body scroll bug

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { type Node } from "react";
+import React, { Component, type Node } from "react";
 import ReactModal from "react-modal";
 import styled from "styled-components";
 
@@ -56,61 +56,68 @@ type Props = {|
     onClose: () => void,
 |};
 
-const Modal = ({ children, isOpen, onClose }: Props) => (
-    <ReactModal
-        isOpen={isOpen}
-        onRequestClose={() => {
-            // $FlowFixMe
-            document.body.style.overflow = null;
-            onClose();
-        }}
-        onAfterOpen={() => {
-            // $FlowFixMe
-            document.body.style.overflow = "hidden";
-        }}
-        style={{
-            overlay: {
-                // zIndex: 100001,
-                display: "flex",
-                flexFlow: "row nowrap",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "rgba(230, 230, 230, 0.75)",
-            },
-            content: {
-                // remove defaults
-                position: undefined,
-                top: undefined,
-                left: undefined,
-                right: undefined,
-                bottom: undefined,
-                border: null,
-                padding: null,
-                borderRadius: "1em",
-                boxShadow: "0px 0px 1.1em 2px rgba(0, 0, 0, 0.2)",
-                maxWidth: "95%",
-                maxHeight: "95%",
-                minWidth: "60%",
-                display: "flex",
-                flexFlow: "column nowrap",
-                justifyContent: "center",
-                alignItems: "center",
-            },
-        }}
-    >
-        <Main>
-            <Header>
-                <Button onClick={onClose}>
-                    <ClosePadding>
-                        <Close>
-                            <CloseImage src={close} />
-                        </Close>
-                    </ClosePadding>
-                </Button>
-            </Header>
-            <Children>{children}</Children>
-        </Main>
-    </ReactModal>
-);
-
+class Modal extends Component<Props> {
+    close = () => {
+        // $FlowFixMe
+        document.body.style.overflow = null;
+        this.props.onClose();
+    };
+    render() {
+        const { children, isOpen, onClose } = this.props;
+        return (
+            <ReactModal
+                isOpen={isOpen}
+                onRequestClose={() => {
+                    this.close();
+                }}
+                onAfterOpen={() => {
+                    // $FlowFixMe
+                    document.body.style.overflow = "hidden";
+                }}
+                style={{
+                    overlay: {
+                        // zIndex: 100001,
+                        display: "flex",
+                        flexFlow: "row nowrap",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        backgroundColor: "rgba(230, 230, 230, 0.75)",
+                    },
+                    content: {
+                        // remove defaults
+                        position: undefined,
+                        top: undefined,
+                        left: undefined,
+                        right: undefined,
+                        bottom: undefined,
+                        border: null,
+                        padding: null,
+                        borderRadius: "1em",
+                        boxShadow: "0px 0px 1.1em 2px rgba(0, 0, 0, 0.2)",
+                        maxWidth: "95%",
+                        maxHeight: "95%",
+                        minWidth: "60%",
+                        display: "flex",
+                        flexFlow: "column nowrap",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    },
+                }}
+            >
+                <Main>
+                    <Header>
+                        <Button onClick={this.close}>
+                            <ClosePadding>
+                                <Close>
+                                    <CloseImage src={close} />
+                                </Close>
+                            </ClosePadding>
+                        </Button>
+                    </Header>
+                    <Children>{children}</Children>
+                </Main>
+            </ReactModal>
+        );
+    }
+}
 export default Modal;
