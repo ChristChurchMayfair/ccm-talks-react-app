7a21e2ea7c2a971232f3f0073fada009924f2e63
Tom Duckering <tduckering@apple.com>
2018-09-12 22:17:05 +0100

    Remove featured sermons completely.
    Recent are at the top. So that kinda works for now

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,6 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import Spinner from "react-spinkit";
 
-import FeaturedSermons from "../FeaturedSermons";
 import SeriesList from "../SeriesList";
 import WithSerieses from "../WithSerieses";
 import Modal from "../Modal";
@@ -58,15 +57,7 @@ class App extends Component<Props, State> {
                     );
                     return (
                         <div>
-                            <h1>Featured</h1>
-                            <FeaturedSermons
-                                serieses={serieses}
-                                featuredSermonIds={[
-                                    "cjhoq9p0g3g7w01103xr223pe",
-                                    "cjhoq9ow735eu0186s5s6e2v3",
-                                ]}
-                            />
-                            <h1>Archive</h1>
+                            <h1>Latest Talks</h1>
                             <SeriesList
                                 serieses={serieses}
                                 onSelectSeries={this.selectSeries}
